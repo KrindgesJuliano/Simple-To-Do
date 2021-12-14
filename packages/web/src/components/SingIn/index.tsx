1a7eb22821a17a23bcf65ef4a075059572cfdb25
Juliano Krindges <juliano9krindges@hotmail.com>
2021-12-14 09:32:54 -0400

    feat: add axios e algumas pequenas mudanças

--- packages/web/src/components/SingIn/index.tsx
+++ packages/web/src/components/SingIn/index.tsx
@@ -4,13 +4,18 @@ interface Props {}
 
 export function SingIn({}: Props): ReactElement {
   return (
-    <div className="container mx-auto flex h-[50rem] justify-center items-center">
-      <div className="flex flex-col w-[34rem] h-60 p-4 border-[1px] border-gray-400 rounded">
-        <p>Crie seu login</p>
-        <label htmlFor="username">Nome</label>
-        <input type="text" id="username" />
-        <label htmlFor="username">email</label>
-        <input type="email" name="" id="email" />
+    <div className="container mx-auto h-[50rem] grid grid-cols-2">
+      <div className="bg-red-200">
+        <h1>Para continuar crie uma conta</h1>
+      </div>
+      <div className="flex justify-center items-center ">
+        <div className="w-1/2 h-auto p-4 flex gap-3 flex-col">
+          <label htmlFor="username">Nome</label>
+          <input type="text" id="username" className="h-10" />
+          <label htmlFor="username">email</label>
+          <input type="email" name="" id="email" className="h-10" />
+          <input type="button" value="Criar Conta" />
+        </div>
       </div>
     </div>
   )
